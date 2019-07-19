70f95619eaaf874aca0d9577fb75f3084c0cec67
bocahrokok <nurrizkyimani@mail.ugm.ac.id>
2019-07-19 15:24:43 +0000

    warga indie malam

--- FirstExpressApp/solution/app.js
+++ FirstExpressApp/solution/app.js
@@ -13,11 +13,16 @@ app.get("/speak/:animal", function(req, res){
 		cow: "Moo",
 		dog: "Woof woff!",
 		cat: "I hate you human",
-	},
-	var animal = req.params.animal;
+	};
+	var animal = req.params.animal.toLowerCase();
 	var sound = sound[animal];
-
 	res.send("The" + animal + "says" + sound );
+});
+
+app.get("/repeat/:message/:times", function(req, res){
+	var message = req.params.message;
+	var times = req.params.times;
+	res.send("MessageL " + message +  "times" + times);
 });
 
 app.listen(3000, function(){
